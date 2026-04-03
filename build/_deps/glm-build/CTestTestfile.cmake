233c47df438885bb37dac9405af7596a376c30e0
Rahul Tudu <rahultudu4705@gmail.com>
2026-04-04 00:54:50 +0530

    Remove build from tracking and update gitignore

--- build/_deps/glm-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rahul/.projects/inertialink/build/_deps/glm-src
-# Build directory: /home/rahul/.projects/inertialink/build/_deps/glm-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
